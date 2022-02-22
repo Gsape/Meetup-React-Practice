8a7c7560cc039a2fe4fec6814af57f64508d80d7
Grant S <grant.sapir@gmail.com>
2022-02-22 12:13:20 -0500

    Route start

--- meetup-react-practice/src/App.js
+++ meetup-react-practice/src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+
+import AllMeetupsPage from "./pages/AllMeetUps";
+import NewMeetupsPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
-  return( 
-    <div>Hello!</div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
   );
 }
 
