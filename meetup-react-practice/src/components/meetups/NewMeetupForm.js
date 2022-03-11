aa4fa21ef52286c22b0dec4c65cc37433fc71341
Grant S <grant.sapir@gmail.com>
2022-03-11 10:59:26 -0500

    form navigate submission and Firebase addition

--- meetup-react-practice/src/components/meetups/NewMeetupForm.js
+++ meetup-react-practice/src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,8 @@ import { useRef } from "react";
 
 import Card from "../UI/Card";
 import classes from "./NewMeetupForm.module.css";
-function NewMeetupForm() {
+
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -23,7 +24,7 @@ function NewMeetupForm() {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- meetup-react-practice/src/pages/NewMeetUp.js
+++ meetup-react-practice/src/pages/NewMeetUp.js
@@ -1,10 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-app-faae7-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method:'POST',
+        body: JSON.stringify(meetupData),
+        header: {
+          'Content-Type': 'application/json'
+        }
+      } 
+    ).then(() => {
+      navigate('/',{replace:true});
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
